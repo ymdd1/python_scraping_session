cd811ee8051d5cc9e7413a1c397f48d4a411257f
Daichi Yamada <dy.mcbzsence00w@gmail.com>
2022-11-22 12:21:07 +0900

    feat: フォーマッター追加

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python_scraping_session
 python勉強会　ドキュメント
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
--- codes/scraping/views.py
+++ codes/scraping/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 
+
 def post_list(request):
     target_url = request.GET.get("target_url")
-    html_text = requests.get(target_url,verify=False).text
-    origin_html = BeautifulSoup(html_text, 'html.parser')
+    html_text = requests.get(target_url, verify=False).text
+    origin_html = BeautifulSoup(html_text, "html.parser")
 
-    result = origin_html.find_all(class_='gnav_label')
-    return render(request, 'scraping/post_list.html', {'target_url': target_url, 'result': result, 'origin_html': origin_html.prettify()})
+    result = origin_html.find_all(class_="gnav_label")
+    return render(
+        request,
+        "scraping/post_list.html",
+        {
+            "target_url": target_url,
+            "result": result,
+            "origin_html": origin_html.prettify(),
+        },
+    )
