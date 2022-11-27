d043e831ce930b8f12b9a86fae36be41e360a941
Daichi Yamada <dy.mcbzsence00w@gmail.com>
2022-11-27 22:16:23 +0900

    feat: コメント追加

--- codes/scraping/views.py
+++ codes/scraping/views.py
@@ -5,16 +5,33 @@ from bs4 import BeautifulSoup
 
 def post_list(request):
     target_url = request.GET.get("target_url")
-    html_text = requests.get(target_url, verify=False).text
-    origin_html = BeautifulSoup(html_text, "html.parser")
 
-    result = origin_html.find_all(class_="gnav_label")
+    if not target_url:
+        return render(
+            request,
+            "scraping/post_list.html",
+            {
+                "target_url": "",
+                "result": "",
+                "origin_html": "",
+            },
+        )
+
+    # 指定したページのhtmlを取得
+    html_text = requests.get(target_url, verify=False).text
+
+    # BeautifulSoupオブジェクトに変換
+    soup = BeautifulSoup(html_text, "html.parser")
+
+    # データ抽出ロジック
+    result = soup.find_all(class_="gnav_label")
+
     return render(
         request,
         "scraping/post_list.html",
         {
             "target_url": target_url,
             "result": result,
-            "origin_html": origin_html.prettify(),
+            "origin_html": soup.prettify(),
         },
     )
